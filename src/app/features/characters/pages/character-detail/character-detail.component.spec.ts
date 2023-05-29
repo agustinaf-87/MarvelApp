5683b9f605a5d30827ea4e668542189fc964260f
agustinaf-87 <fernandez.agustinaelena@gmail.com>
2023-05-29 13:04:30 +0200

    Added landing presentation closes #1

--- src/app/features/characters/pages/character-detail/character-detail.component.spec.ts
+++ src/app/features/characters/pages/character-detail/character-detail.component.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { CharacterDetailComponent } from './character-detail.component';
+import { CharacterDetailComponent } from "./character-detail.component";
 
-describe('CharacterDetailComponent', () => {
+describe("CharacterDetailComponent", () => {
   let component: CharacterDetailComponent;
   let fixture: ComponentFixture<CharacterDetailComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CharacterDetailComponent ]
-    })
-    .compileComponents();
+      declarations: [CharacterDetailComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CharacterDetailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
