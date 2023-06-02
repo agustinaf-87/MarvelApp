75c26c99b65f47d740db300056f5b8dcf5289d0b
agustinaf-87 <fernandez.agustinaelena@gmail.com>
2023-06-02 18:28:50 +0200

    uploading changes on comic page carousel

--- src/app/features/comics/comics.module.ts
+++ src/app/features/comics/comics.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from "@angular/common";
 import { ComicsRoutingModule } from "./comics-routing.module";
 import { ComicsComponent } from "./comics.component";
 import { ComicCardComponent } from "./components/comic-card/comic-card.component";
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 @NgModule({
-  declarations: [ComicsComponent, ComicCardComponent],
+  declarations: [ComicsComponent, ComicCardComponent, CarouselComponent],
   imports: [CommonModule, ComicsRoutingModule],
 })
 export class ComicsModule {}
