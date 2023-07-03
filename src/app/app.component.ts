5a29c9060f4872254c58bf607f19b37933105ee3
agustinaf-87 <fernandez.agustinaelena@gmail.com>
2023-07-03 16:47:19 +0200

    message in develop environment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { environment } from "src/environments/environment.development";
 
 @Component({
   selector: "app-root",
@@ -7,4 +8,5 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "MarvelExam";
+  code = environment.code;
 }
