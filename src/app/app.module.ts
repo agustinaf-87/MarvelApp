ad67ecdd60a9e8e3d0dce966a11a1d2fe7168e27
agustinaf-87 <fernandez.agustinaelena@gmail.com>
2023-07-03 14:57:48 +0200

    uploading changes on app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,15 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import {
-  HTTP_INTERCEPTORS,
-  HttpClient,
-  HttpClientModule,
-} from "@angular/common/http";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { CoreModule } from "./core/core.module";
-import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 import { NavigationService } from "./core/services/navigation-service/navigation.service";
 import { LoadingInterceptor } from "./core/interceptor/loading-interceptor/loading.interceptor";
 import { ApiHashAuthInterceptor } from "./core/interceptor/api-hash-auth/api-hash-auth.interceptor";
 import { ErrorInterceptor } from "./core/interceptor/error-interceptor/server-error.interceptor";
 import { ToastrModule } from "ngx-toastr";
-
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
-}
 
 @NgModule({
   declarations: [AppComponent],
